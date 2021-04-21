d48d36c8326afbb741ff1b65f11486bab308172c
CorneliaRoehrer <corne.lia.roehrer@gmail.com>
2021-04-21 13:47:31 +0200

    did the fizz and the buzz

--- basic/exe.php
+++ basic/exe.php
@@ -52,12 +52,27 @@
         echo "The largest number of this array is " .$dummyArray. "<br>";
     }
 
-    // Bettinas version that is much more splendid (and cleaner!) as mine:
+    // Bettinas version that is much more splendid (and cleaner!) than mine:
     // function printMax($c) {
     //     echo (max($c));
     // }
 
     printMax($a);
+
+    // Exercise 4
+    // Create a PHP program that iterates the integers from 1 to 100. For multiples of three print "Back-End" instead of the number and for the multiples of five print "Front-End". For numbers that are multiples of both three and five print "Full-Stack".
+
+    for($i=1; $i<=100; $i++)
+    {
+        if (($i % 3 == 0) && ($i % 5 == 0))
+            echo "Full Stack<br>";
+        if ($i % 3 == 0)
+            echo "Back End<br>";
+        elseif ($i % 5 == 0)
+            echo "Front End<br>";
+        else
+            echo $i."<br>";
+    }
 
     ?>
 </body>
